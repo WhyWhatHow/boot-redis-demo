4e7baeb5e7fc2e8c2bc29fc9d8235fd1484d05dc
whywhathow <anothernash@126.com>
2021-01-04 13:49:35 +0800

    Time: 2 content: goodsController  实现:  单机情况下的加锁行为(java层面),未出现任何错误  缺点: 在服务集群化(service 1111,2222)的情况下,出现超卖现象 redisController:  实现: 在单个 redis 缓存系统的情况下,即nginx,集群(service1111,service2222),redis情境下, 未出现超卖行为  缺点: 未处理redis 集群下的情况

--- src/main/java/io/github/whywhathow/bootredisdemo/config/controller/GoodsController.java
+++ src/main/java/io/github/whywhathow/bootredisdemo/config/controller/GoodsController.java
@@ -7,9 +7,11 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @program: boot-redis-demo
- * @description:
+ * @description: 单机情况下, 通过添加java 自带的锁可以实现,但是 在分布式系统环境下, 会出现重买,超卖的现象
  * @author: WhyWhatHow
  * @create: 2020-12-25 14:15
  **/
@@ -22,21 +24,77 @@ public class GoodsController {
     @Value("${server.port}")
     private String serverPort;
 
+
+    /**
+     * @Description: 单机, 无锁情况,在高并发情况下 出现超卖
+     * @return: java.lang.String
+     * @Author: WhyWhatHow
+     * @Date: 2020/12/25 15:07
+     **/
     @GetMapping("/buy_goods")
-    public String buy_Goods(){
+    public String buy_Goods() {
+        return buy();
+    }
+
+    Object syncLock = new Object();
+
+    /**
+     * @Description:
+     *  单机, synchronized 锁情况下, 未出现超卖情况
+     *  集群情况下: 失败 出现超卖现象(原因  nginx->tomcat1111(自己的锁),tomcat2222(自己的锁))
+     *  solve: 锁定一个公共变量充当锁, 然后让两个server 去竞争这把锁,已避免超卖情况.
+     *
+     * @Param:
+     * @return:
+     * @Author: WhyWhatHow
+     * @Date: 2020/12/25 15:08
+     **/
+    @GetMapping("/sync/buy_goods")
+    public String buy_GoodsBySynchronized() {
+        synchronized (syncLock) {
+            return buy();
+        }
+    }
+
+    ReentrantLock lock = new ReentrantLock();
+
+    @GetMapping("/re/buy_goods")
+    public String buy_GoodsByReentrantLock() {
+        if (lock.tryLock()) {
+            lock.lock();
+            try {
+                String buy = buy();
+                return buy;
+            } finally {
+                lock.unlock();
+            }
+
+
+        } else {
+            return "当前人数过多,请稍后再试!!!!!! ";
+        }
+
+    }
+
+    /***
+     * 模拟 购买某一固定商品(goods:001)的过程
+     * @return
+     */
+    private String buy() {
 
         String result = stringRedisTemplate.opsForValue().get("goods:001");
         int goodsNumber = result == null ? 0 : Integer.parseInt(result);
 
-        if (goodsNumber > 0){
+        if (goodsNumber > 0) {
             int realNumber = goodsNumber - 1;
-            stringRedisTemplate.opsForValue().set("goods:001",realNumber + "");
-            System.out.println("你已经成功秒杀商品，此时还剩余：" + realNumber + "件"+"\t 服务器端口: "+serverPort);
-            return "你已经成功秒杀商品，此时还剩余：" + realNumber + "件"+"\t 服务器端口: "+serverPort;
-        }else {
-            System.out.println("商品已经售罄/活动结束/调用超时，欢迎下次光临"+"\t 服务器端口: "+serverPort);
+            stringRedisTemplate.opsForValue().set("goods:001", realNumber + "");
+            System.out.println("你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort);
+            return "你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort;
+        } else {
+            System.out.println("商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort);
         }
-        return "商品已经售罄/活动结束/调用超时，欢迎下次光临"+"\t 服务器端口: "+serverPort;
+        return "商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort;
     }
+
 
 }
---
+++ src/main/java/io/github/whywhathow/bootredisdemo/config/controller/RedisSetNxGoodsController.java
@@ -0,0 +1,140 @@
+package io.github.whywhathow.bootredisdemo.config.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @program: boot-redis-demo
+ * @description: 处理分布式系统下的情况....
+ * @author: WhyWhatHow
+ * @create: 2020-12-25 14:15
+ **/
+@RestController
+@Slf4j
+@RequestMapping("/redis/setnx/")
+public class RedisSetNxGoodsController {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+
+    /**
+     * @Description: 微服务集群 , redis 单机情况满足, 若是redis集群存在问题... redis-setnx
+     * @return: java.lang.String
+     * @Author: WhyWhatHow
+     * @Date: 2020/12/25 15:07
+     **/
+    @GetMapping("/buy_goods")
+    public String buy_GoodsPart1() {
+        String val = UUID.randomUUID().toString() + Thread.currentThread().getName();
+        String result = "";
+        try {
+            // 1. 尝试获取锁,redis_setnx( key   , value , ttl )
+            // 设置超时时间, 避免因为 当前服务进程出错,导致无法删除
+            Boolean aBoolean = stringRedisTemplate.opsForValue().setIfAbsent(REDIS_SETNX_LOCK, val, Duration.ofSeconds(10));
+            if (aBoolean) {
+                // 2. 获取锁成功, shopping ,失败则返回提示信息
+                result = setnxBy();
+            } else {
+                result = " 获取锁失败...页面繁忙, 请稍后重试. serverPort: " + serverPort;
+            }
+        } catch (Exception e) {
+            // // TODO: 2021/1/3 异常出错, 需要释放锁
+        } finally {
+            // 3.  主动释放锁, 不主动释放锁的话 ,会在超时时间结束后释放锁
+            // 3.1 判断锁 是否被当前线程所占有, 避免 删除其他线程的锁情况
+            String s = stringRedisTemplate.opsForValue().get(REDIS_SETNX_LOCK);
+            //
+            if (val.equals(s)) {
+                //3.2  删除当前线程所占有的锁
+                // 3.2.1  非原子性 删除
+//                stringRedisTemplate.delete(REDIS_SETNX_LOCK);
+                // 3.2.2 原子性删除 ... lua
+                unLockByLua(val, REDIS_SETNX_LOCK);
+                // 3.2.3 redis 事务删除
+//                redisTransactionDel(val);
+
+            }
+
+        }
+
+        return result;
+    }
+
+    private void redisTransactionDel(String val) {
+        while (true){
+            if (val.equals(stringRedisTemplate.opsForValue().get(REDIS_SETNX_LOCK))) {
+                stringRedisTemplate.watch(REDIS_SETNX_LOCK);
+                stringRedisTemplate.setEnableTransactionSupport(true);
+                stringRedisTemplate.multi();
+                stringRedisTemplate.delete(REDIS_SETNX_LOCK);
+                List<Object> exec = stringRedisTemplate.exec();
+                if (exec.isEmpty()) {
+                    continue;
+                }
+                stringRedisTemplate.unwatch();
+                break;
+            }
+        }
+    }
+
+    /**
+     * 原子操作,  redis eval luascript
+     * 解锁
+     *
+     * @param val
+     * @param redisLock
+     */
+    private void unLockByLua(String val, String redisLock) {
+        String luaScript = "if redis.call(\"get\",KEYS[1]) == ARGV[1]\n" +
+                "then\n" +
+                "    return redis.call(\"del\",KEYS[1])\n" +
+                "else\n" +
+                "    return 0\n" +
+                "end";
+        DefaultRedisScript<Long> objectDefaultRedisScript = new DefaultRedisScript<>(luaScript, Long.class);
+        stringRedisTemplate.execute(objectDefaultRedisScript, Collections.singletonList(redisLock), val);
+    }
+
+
+    /***
+     * 利用 redis 自带命令setnx(if key not exist ,then set val )
+     * 模拟 购买某一固定商品(goods:001)的过程
+     * @return
+     */
+    String REDIS_SETNX_LOCK = "redisSetnxLock";
+
+    private String setnxBy() {
+
+
+        String result = stringRedisTemplate.opsForValue().get("goods:001");
+        int goodsNumber = result == null ? 0 : Integer.parseInt(result);
+
+        if (goodsNumber > 0) {
+            int realNumber = goodsNumber - 1;
+            stringRedisTemplate.opsForValue().set("goods:001", realNumber + "");
+            System.out.println("你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort);
+            return "你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort;
+        } else {
+            System.out.println("商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort);
+        }
+        return "商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort;
+    }
+
+
+}
