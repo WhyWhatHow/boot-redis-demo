0a6b5512df9df04c59e17fe2c791c9c2ab4e1360
whywhathow <anothernash@126.com>
2021-05-20 21:29:54 +0800

    Time: 4 content: 给了 项目文档地址, 以及使用说明.

--- src/main/java/io/github/whywhathow/bootredisdemo/BootRedisDemoApplication.java
+++ src/main/java/io/github/whywhathow/bootredisdemo/BootRedisDemoApplication.java
@@ -6,7 +6,21 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude ={DataSourceAutoConfiguration.class})
 public class BootRedisDemoApplication {
-
+    /**
+     * 项目详细说明:
+     *     文档地址:https://www.whywhathow.fun/projects/projects-01-redis-distributedLock/
+     * 大概还有五个案例:
+     * 分别是:
+     *      1.单机不加锁: http://localhost:1111/buy_goods/{goodsId}
+     *      2. 单机加锁:  http://localhost:1111/sync/buy_goods/{id}
+     *      3. 分布式加锁--事务,lua 原子操作模式:  http://localhost:1111/redis/setnx/buy_goods/{id}
+     *      4. 分布式加锁-- redisson 分布式锁: http://localhost:1111/redisson/buy_goods/{id}
+     *
+     * PS: 建议项目用idea 工具打开, 并安装 RestService  插件(直接在插件市场中搜索就好) ,
+     *     并参考文档地址
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(BootRedisDemoApplication.class, args);
     }
